e1e5481a216d58e8ba7588e5213fa5f89ca69eec
Deniz Uysal <deniz1648@icloud.com>
2023-12-08 14:14:52 +0000

    feat: first try

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,15 @@
 """Forms of the project."""
 
 # Create your forms here.
+# things/forms.py
+from django import forms
+from .models import Thing
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model = Thing
+        exclude = ['created_at']  # Exclude the 'created_at' field from the form
+        widgets = {
+            'description': forms.Textarea(),  # Display 'description' as a Textarea
+            'quantity': forms.NumberInput(),  # Display 'quantity' as NumberInput
+        }
--- things/views.py
+++ things/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+# views.py
+from django.shortcuts import render
+from .forms import ThingForm
 
 def home(request):
+    if request.method == 'POST':
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            form.save()  # Save the valid form data
+            # Redirect or do something else after successful form submission
+            # Example: return redirect('success_url')
+    else:
+        form = ThingForm()
+
     return render(request, 'home.html', {'form': form})
